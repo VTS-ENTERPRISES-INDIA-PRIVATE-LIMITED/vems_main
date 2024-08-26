2ccd2d3d811c1adf782bbbb7ed143c56e04a38cc
CharanKondaveeti <91211319+CharanKondaveeti@users.noreply.github.com>
2024-08-26 12:23:34 +0530

    knj
    hu

--- src/Components/SortFilteringTrips/SortFilteringTrips.jsx
+++ src/Components/SortFilteringTrips/SortFilteringTrips.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="headers">
       <div className="status-info">
-        <p className="status-total">210 All</p>
+        <p className="status-total hh">210 All</p>
         <p className="status-running">Running: 150</p>
         <p className="status-stopped">Stopped: 20</p>
         <p className="status-unreachable">Unreachable: 15</p>
